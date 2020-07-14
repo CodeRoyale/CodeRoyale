38059e9fb81d215454b7b40dfa938386e1ee956a
Sawarni Swaroop <sawarni99@gmail.com>
2020-07-14 21:18:34 +0530

    Created Directories for lobby

---
+++ src/components/lobby/Lobby.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Lobby() {
+  return <div>{/* TODO: This is where lobby will be...*/}</div>;
+}
+
+export default Lobby;
---
+++ src/components/lobby/lobby-components/start-button/StartButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StartButton() {
+  return <div>{/*TODO: Button to start the match... */}</div>;
+}
+
+export default StartButton;
