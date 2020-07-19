e392027eb6fdbba84b3d6d5f028d7c842fbc4b9c
Coming___soON <sachinvilasnagane@karunya.edu.in>
2020-07-20 01:15:24 +0530

    Swagger-JDdocs

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
-//basic setting up
+// basic setting up
+/* eslint-disable global-require */
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+/* eslint-enable global-require */
 
-//importing packages
+const PORT = process.env.PORT || 3000;
+
+// importing packages
 const express = require('express');
 const os = require('os');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http');
 const cookieParser = require('cookie-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const path = require('path');
 
-//connect to DB
+// connect to DB
 (async () => {
   try {
     await mongoose.connect(
@@ -31,18 +35,18 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-//middlewares
+// middlewares
 app.use(cookieParser());
 
-var whitelist = ['http://localhost:3000', 'https://codeRoyale.herokuapp.com'];
-var corsOptions = {
+const whitelist = ['http://localhost:3000', 'https://codeRoyale.herokuapp.com'];
+const corsOptions = {
   origin: function (origin, callback) {
-    //add !origin for services like postman
+    // add !origin for services like postman
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      //i dont like this it logs the shit
-      //-can use customErrorHandler
+      // i dont like this it logs the shit
+      // -can use customErrorHandler
       callback(new Error('Not allowed by CORS.'));
     }
   },
@@ -53,13 +57,43 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-//Routes
+// Routes
 app.use('/', require('./routes/main'));
 
-//start listening
+// start listening
 const server = app.listen(PORT, () => {
   const host = os.hostname();
   console.log('Server Started at ', host, ':', server.address().port);
+});
+
+// parameter for swaggerJSDoc to render the json for redoc
+const options = {
+  definition: {
+    components: {},
+    openapi: '3.0.0',
+    info: {
+      title: 'CodeRoyale API Documentation',
+      version: 'v1.0.0',
+      description:
+        '<h3>Introduction</h3> <br/>The goal of CodeRoyale is to create Competitive Programming platform where users can have coding battle with friends, random users and team battles to show case your DSA skills.<hr/>',
+    },
+  },
+  // Path to the files where API documentations are written
+  apis: [path.resolve(__dirname, '../_docs/*.js')],
+};
+
+// passing option as a parameter to swaggerJSDoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// request for getting the json file rendered by swaggerJSDocs
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+// request for seeing the api-documentation live
+app.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, 'redoc.html'));
 });
 
 module.exports = {
