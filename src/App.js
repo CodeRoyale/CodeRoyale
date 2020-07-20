4266294003e6f28c1e6f6d27f0000734cb3aaf31
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-20 12:06:53 +0530

    Inital commit for front-page revamp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Lobby from './components/lobby/Lobby';
-import FrontPage from './components/frontPage/FrontPage';
+import FrontPage from './pages/frontPage/FrontPageMain';
 
 function App() {
   return (
---
+++ src/pages/frontPage/FrontPageMain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FrontMain extends Component {
+  render() {
+    return (
+      <div>
+        <h1>FrontPage</h1>
+      </div>
+    );
+  }
+}
+
+export default FrontMain;
