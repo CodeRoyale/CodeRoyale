f00eaa10d6c4db301aa8524cfe4ddf82e794fc99
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-21 20:49:10 +0530

    Added login page

--- src/pages/loginPage/LoginMain.js
+++ src/pages/loginPage/LoginMain.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './LoginMain.css';
+import LeftSection from './leftSection/LeftSection';
+import LoginSection from './loginSection/LoginSection';
 
 class LoginMain extends Component {
   render() {
     return (
       <div className='login-page'>
-        <div className='login-left-section'>
-          <h1>joel</h1>
-        </div>
+        <LeftSection />
+        <LoginSection />
       </div>
     );
   }
