72135e50b21b9284af8f2a30e9ad7cb5baa45ef1
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-21 13:57:40 +0530

    Added login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Lobby from './components/lobby/Lobby';
 import FrontPage from './pages/frontPage/FrontPageMain';
+import LoginMain from './pages/loginPage/LoginMain';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Switch>
+          <Route exact path='/login' component={LoginMain} />
           <Route exact path='/lobby' component={Lobby} />
           <Route exact path='' component={FrontPage} />
           <Route exact path='/' component={FrontPage} />
---
+++ src/pages/loginPage/LoginMain.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './LoginMain.css';
+
+class LoginMain extends Component {
+  render() {
+    return (
+      <div className='login-page'>
+        <div className='login-left-section'>
+          <h1>joel</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginMain;
