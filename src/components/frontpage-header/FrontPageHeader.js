0f44d8a880cd44b03d892151dbe2cdd0808ccfbf
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-21 13:32:13 +0530

    Drop down menu

--- src/components/frontpage-header/FrontPageHeader.js
+++ src/components/frontpage-header/FrontPageHeader.js
@@ -7,7 +7,35 @@ function FrontPageHeader() {
     <div className='frontpage-header'>
       <h2 className='coderoyale-title'>CodeRoyale</h2>
       <div className='frontpage-header-links'>
-        <p>Play</p>
+        <ul>
+          <li>
+            Play
+            <div className='play-menu'>
+              <div className='play-menu-options'>
+                <div className='play-item-with-friend'>
+                  <img
+                    alt='programming'
+                    src='images/friends.svg'
+                    width='150px'
+                    height='150px'
+                  />
+                  <h4>Play with a friend</h4>
+                  <p>Challenge a friend to a 1v1 coding competition.</p>
+                </div>
+                <div className='play-item-with-random'>
+                  <img
+                    alt='programming'
+                    src='images/random.svg'
+                    width='150px'
+                    height='150px'
+                  />
+                  <h4>Play with a random coder</h4>
+                  <p>Compete with a random coder from around the globe.</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
       <AuthButton text='Sign in' />
     </div>
