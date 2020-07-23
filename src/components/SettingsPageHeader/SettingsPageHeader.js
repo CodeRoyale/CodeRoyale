59c4e6db0717141cce15bbfd9c8b0fd9c2628ee3
alanhenry0221 <alanhenry0221@gmail.com>
2020-07-23 09:52:46 +0530

    added header and illustration

---
+++ src/components/SettingsPageHeader/SettingsPageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SettingsPageHeader.css';
+
+function FrontPageHeader() {
+  return (
+    <div className='settings-header'>
+      <h2 className='coderoyale-title'>CodeRoyale</h2>
+    </div>
+  );
+}
+
+export default FrontPageHeader;
--- src/pages/profileSettingsPage/ProfileSettingsMain.js
+++ src/pages/profileSettingsPage/ProfileSettingsMain.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/header/Header';
+import SettingsHeader from '../../components/SettingsPageHeader/SettingsPageHeader';
 import './ProfileSettingsMain.css';
 
 const userEmailId = 'recoilop@gmail.com'; //to be fetched from api
@@ -7,30 +7,41 @@ const userEmailId = 'recoilop@gmail.com'; //to be fetched from api
 function ProfileSettingsMain() {
   return (
     <div>
-      <Header></Header>
+      <SettingsHeader />
+      <div className='settings-container'>
+        <div>
+          <div className='settings-title'>Profile Settings</div>
 
-      <div className='settings-title'>Profile Settings</div>
+          <div className='options-header'>First Name</div>
+          <div>
+            <input type='text' size='90' className='options-input'></input>
+          </div>
+          <div className='options-header'>Last Name</div>
+          <div>
+            <input type='text' size='90' className='options-input'></input>
+          </div>
+          <div className='options-header'>Email Id</div>
+          <div>
+            <input
+              type='text'
+              size='90'
+              disabled
+              placeholder={userEmailId}
+              className='options-input'
+            ></input>
+          </div>
 
-      <div className='options-header'>First Name</div>
-      <div>
-        <input type='text' size='90' className='options-input'></input>
-      </div>
-      <div className='options-header'>Last Name</div>
-      <div>
-        <input type='text' size='90' className='options-input'></input>
-      </div>
-      <div className='options-header'>Email Id</div>
-      <div>
-        <input
-          type='text'
-          size='90'
-          disabled
-          placeholder={userEmailId}
-          className='options-input'
-        ></input>
-      </div>
+          <button className='save-button'>Save Settings</button>
+        </div>
 
-      <button className='save-button'>Save Settings</button>
+        <div className='setting-separator' />
+
+        <img
+          className='settings-image'
+          alt='setting illustration'
+          src='/images/settings.svg'
+        />
+      </div>
     </div>
   );
 }
