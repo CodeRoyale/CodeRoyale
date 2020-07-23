c151f2f31cf42c63ee79a80b2fb1639f42b3e482
Sawarni Swaroop <sawarni99@gmail.com>
2020-07-23 18:06:09 +0530

    Created merged files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import FrontPage from './components/frontPage/FrontPage';
+// import FrontPage from './components/frontPage/FrontPage';
 import DashboardMain from './pages/dashboardPage/DashboardMain';
 import LobbyMain from './pages/lobbyPage/LobbyMain';
+// import Lobby from './components/lobby/Lobby';
+import FrontPage from './pages/frontPage/FrontPageMain';
+import LoginMain from './pages/loginPage/LoginMain';
+import ProfileSettingsMain from './pages/profileSettingsPage/ProfileSettingsMain';
 
 function App() {
   return (
@@ -12,6 +16,9 @@ function App() {
         <Switch>
           <Route exact path='/lobby' component={LobbyMain} />
           <Route exact path='/dashboard' component={DashboardMain} />
+          <Route exact path='/login' component={LoginMain} />
+          <Route exact path='/lobby' component={LobbyMain} />
+          <Route exact path='/settings' component={ProfileSettingsMain} />
           <Route exact path='' component={FrontPage} />
           <Route exact path='/' component={FrontPage} />
         </Switch>
