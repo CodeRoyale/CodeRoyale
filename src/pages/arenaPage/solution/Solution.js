7a656b71bd8051da0b8e80cc1301e24c638f2817
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-27 13:29:37 +0530

    Added mode and theme

--- src/pages/arenaPage/solution/Solution.js
+++ src/pages/arenaPage/solution/Solution.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Solution.css';
 import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/theme-github';
 
 function solution() {
   return (
     <div className='solution-body'>
       <div className='solution-header'>SOLUTION</div>
       <div className='solution-content'>
-        <AceEditor height='100%' width='100%' />
+        <AceEditor height='100%' width='100%' mode='java' theme='github' />
       </div>
     </div>
   );
