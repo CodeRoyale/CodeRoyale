64e913b61905edabe1082f8bd9b65e619eb1de30
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-29 19:46:25 +0530

    Connection with api

--- src/pages/signUpPage/SignUpSec.js
+++ src/pages/signUpPage/SignUpSec.js
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GoogleSignIn from '../../components/googleSignIn/GoogleSignIn';
 import { Link } from 'react-router-dom';
 import './SignUpMain.css';
 
-function SignUpSec() {
-  return (
-    <div className='signup-section-container'>
-      <div className='signup-section-content'>
-        <center>
-          <p className='signup-section-title'>Sign up for CodeRoyale</p>
-          <GoogleSignIn text='Sign up with Google' />
-          <p className='signup-section-sign-up'>
-            Already a member?{' '}
-            <Link to='/signin' style={{ textDecoration: 'none' }}>
-              <span className='span-text'>Sign in now</span>
-            </Link>
-          </p>
-        </center>
+class SignUpSec extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      googleData: {},
+    };
+  }
+
+  handleGoogleData = (data) => {
+    this.setState({ googleData: data });
+    console.log(this.state.googleData.wc.id_token);
+  };
+
+  sendToServer = () => {
+    // let headers = new Headers();
+    // headers.append('Content-Type', 'application/json');
+    // headers.append('Origin', 'http://localhost:3000');
+    // headers.append('Access-Control-Allow-Credentials', 'true');
+
+    console.log('Sending data..');
+    fetch('http://localhost:5000/users/signup', {
+      method: 'POST',
+      // headers,
+      body: JSON.stringify({
+        thirdPartyData: {
+          issuer: 'google',
+          signUpType: 'native',
+          idToken: this.state.googleData.wc.id_token,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((jsonRes) => {
+        // Success response from server
+        console.log(jsonRes);
+      })
+      .catch((err) => {
+        // Error response from server
+        console.log(err);
+      });
+    console.log('Got response back..');
+  };
+
+  render() {
+    return (
+      <div className='signup-section-container'>
+        <div className='signup-section-content'>
+          <center>
+            <p className='signup-section-title'>Sign up for CodeRoyale</p>
+            <GoogleSignIn
+              text='Sign up with Google'
+              receiveGoogleData={this.handleGoogleData}
+            />
+            <button onClick={this.sendToServer}>Send To server</button>
+            <p className='signup-section-sign-up'>
+              Already a member?{' '}
+              <Link to='/signin' style={{ textDecoration: 'none' }}>
+                <span className='span-text'>Sign in now</span>
+              </Link>
+            </p>
+          </center>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SignUpSec;
