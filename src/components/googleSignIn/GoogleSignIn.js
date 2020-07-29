536203cbd0d84090bf16828f9a0b612fb22ac5dd
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-07-29 19:46:25 +0530

    Connection with api

--- src/components/googleSignIn/GoogleSignIn.js
+++ src/components/googleSignIn/GoogleSignIn.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
 
-class GoogleSignIn extends Component {
-  // Google Client ID from .env file
-  googleClientID = process.env.REACT_APP_CLIENT_ID;
+// Google Client ID from .env file
+const googleClientID = process.env.REACT_APP_CLIENT_ID;
 
+class GoogleSignIn extends Component {
   // This function is called on successful login from google...
   responseSuccess = (response) => {
-    console.log(response);
-    console.log(response.profileObj);
+    this.props.receiveGoogleData(response);
   };
 
   render() {
     return (
       <div>
         <GoogleLogin
-          clientId={this.googleClientID}
+          clientId={googleClientID}
           buttonText={this.props.text}
           onSuccess={this.responseSuccess}
-          onFailure={this.responseFailure}
           cookiePolicy={'single_host_origin'}
         />
       </div>
