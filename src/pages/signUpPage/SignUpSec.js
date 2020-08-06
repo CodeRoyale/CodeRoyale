9dac984e4222fc9e239771769caebb81dce78a85
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-08-06 09:21:57 +0530

    Sign up responses

--- src/pages/signUpPage/SignUpSec.js
+++ src/pages/signUpPage/SignUpSec.js
@@ -39,7 +39,14 @@ class SignUpSec extends Component {
       .then((res) => res.json())
       .then((jsonRes) => {
         // Success response from server
-        // TODO: Show alerts based on response
+        // alerts based on response
+        if (jsonRes.message === 'User Already Exists') {
+        } else if (jsonRes.message === 'User Account Created') {
+        } else if (
+          jsonRes.message === 'Required field missing or Username is in use'
+        ) {
+        } else {
+        }
         console.log(jsonRes);
       })
       .catch((err) => {
