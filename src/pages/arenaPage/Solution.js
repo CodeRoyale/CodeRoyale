09893c447f20983d568e25918daf98e085133f0e
alanhenry0221 <alanhenry0221@gmail.com>
2020-08-07 11:38:46 +0530

    linked submit button to show IDE code on console

--- src/pages/arenaPage/Solution.js
+++ src/pages/arenaPage/Solution.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './ArenaMain.css';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-java';
@@ -12,20 +12,8 @@ import Button from '../../components/button/Button';
 import Popper from '@material-ui/core/Popper';
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import SettingsIcon from '@material-ui/icons/Settings';
-
-const SendCode = () => {
-  console.log('ABCD');
-};
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    border: '1px solid',
-    padding: theme.spacing(1),
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
+import { Row, Col } from 'antd';
 
 const Fade = React.forwardRef(function Fade(props, ref) {
   const { in: open, children, onEnter, onExited, ...other } = props;
@@ -62,16 +50,27 @@ function Solution() {
   const [ideLanguage, setLanguage] = useState('c_cpp');
   const [ideFontSize, setFontSize] = useState('12');
   const [ideTheme, setTheme] = useState('terminal');
+  const [ideCode, setCode] = useState('');
 
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
+  const Alan = useRef('ace');
+
   const open = Boolean(anchorEl);
   const id = open ? 'spring-popper' : undefined;
+
+  function onChangeIDE(newValue) {
+    //console.log('change', newValue);
+    setCode(newValue);
+  }
+
+  const SendCode = () => {
+    console.log(ideCode);
+  };
 
   return (
     <div>
@@ -82,7 +81,7 @@ function Solution() {
           <div className='solution-heading-right'>
             <div className='language-options'>
               <select onChange={(e) => setLanguage(e.target.value)}>
-                <option value='c++'>c++</option>
+                <option value='c_cpp'>c++</option>
                 <option value='java'>java</option>
                 <option value='python'>python</option>
               </select>
@@ -98,43 +97,48 @@ function Solution() {
                 <Popper id={id} open={open} anchorEl={anchorEl} transition>
                   {({ TransitionProps }) => (
                     <Fade {...TransitionProps}>
-                      <div className={classes.paper}>
-                        <div centered divided rows={3}>
-                          <div className='ide-options-row'>
-                            <div>FontSize:</div>
-                            <div>
-                              <select
-                                onChange={(e) =>
-                                  setFontSize(Number(e.target.value))
-                                }
-                              >
-                                <option value='10'>10</option>
-                                <option value='12'>12</option>
-                                <option value='14'>14</option>
-                                <option value='16'>16</option>
-                                <option value='18'>18</option>
-                                <option value='20'>20</option>
-                                <option value='22'>22</option>
-                                <option value='24'>24</option>
-                              </select>
-                            </div>
-
-                            <hr />
-                          </div>
-                          <div className='ide-options-row'>
-                            <div>Theme:</div>
-                            <div>
-                              <select
-                                onChange={(e) => setTheme(e.target.value)}
-                              >
-                                <option value='tomorrow'>tomorrow</option>
-                                <option value='terminal'>terminal</option>
-                                <option value='monokai'>monokai</option>
-                              </select>
-                            </div>
-                            <hr />
-                          </div>
-                        </div>
+                      <div className='ide-options-popup'>
+                        <>
+                          <Row className='ide-options-row'>
+                            <Col span={20}>
+                              <div>FontSize:</div>
+                            </Col>
+                            <Col span={20}>
+                              <div>
+                                <select
+                                  onChange={(e) =>
+                                    setFontSize(Number(e.target.value))
+                                  }
+                                >
+                                  <option value='10'>10</option>
+                                  <option value='12'>12</option>
+                                  <option value='14'>14</option>
+                                  <option value='16'>16</option>
+                                  <option value='18'>18</option>
+                                  <option value='20'>20</option>
+                                  <option value='22'>22</option>
+                                  <option value='24'>24</option>
+                                </select>
+                              </div>
+                            </Col>
+                          </Row>
+                          <Row className='ide-options-row'>
+                            <Col span={20}>
+                              <div>Theme:</div>
+                            </Col>
+                            <Col span={20}>
+                              <div>
+                                <select
+                                  onChange={(e) => setTheme(e.target.value)}
+                                >
+                                  <option value='tomorrow'>tomorrow</option>
+                                  <option value='terminal'>terminal</option>
+                                  <option value='monokai'>monokai</option>
+                                </select>
+                              </div>
+                            </Col>
+                          </Row>
+                        </>
                       </div>
                     </Fade>
                   )}
@@ -146,6 +150,7 @@ function Solution() {
 
         <div id='MyAceEditor' className='solution-content'>
           <AceEditor
+            ref={Alan}
             height='100%'
             width='100%'
             mode={ideLanguage}
@@ -153,6 +158,7 @@ function Solution() {
             fontSize={ideFontSize}
             showGutter={true}
             showPrintMargin={false}
+            onChange={onChangeIDE}
           />
         </div>
       </div>
