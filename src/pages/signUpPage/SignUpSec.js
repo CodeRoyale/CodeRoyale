12a91bac33030e71b014f74fa3501625718b1dc4
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-08-13 10:59:13 +0530

    Added loading for sign up

--- src/pages/signUpPage/SignUpSec.js
+++ src/pages/signUpPage/SignUpSec.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleSignIn from '../../components/googleSignIn/GoogleSignIn';
 import { Link, Redirect } from 'react-router-dom';
 import { message, Spin } from 'antd';
@@ -8,86 +8,115 @@ import './SignUpMain.css';
 
 const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
 const SIGNUP_API = process.env.REACT_APP_SIGNUP_API;
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const ANT_ICON = (
+  <LoadingOutlined style={{ fontSize: 30, color: '#dd2c00' }} spin />
+);
 
-class SignUpSec extends Component {
-  constructor(props) {
-    super(props);
-    const accessToken = localStorage.getItem('access-token');
-    let loggedIn = true;
-    if (accessToken === null) {
-      loggedIn = false;
-    }
-    this.state = {
-      loggedIn,
-      googleData: {},
-    };
+const SignUpSec = () => {
+  const accessToken = localStorage.getItem('access-token');
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const [googleData, setGoogleData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (accessToken != null) {
+    setIsSignedUp(true);
   }
 
-  signUpError = (msg) => {
+  const signUpError = (msg) => {
     message.error(msg);
   };
 
-  signUpSuccess = (msg) => {
+  const signUpSuccess = (msg) => {
     message.success(msg);
   };
 
-  sendToServer = () => {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Origin', CLIENT_URL);
-    headers.append('Access-Control-Allow-Credentials', 'true');
-    // Data to be sent to API
-    const thirdPartyData = {
-      issuer: 'google',
-      signUpType: 'native',
-      idToken: this.state.googleData.wc.id_token,
-    };
-    fetch(SIGNUP_API, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(thirdPartyData),
-    })
-      .then((res) => res.json())
-      .then((jsonRes) => {
-        // Success response from server
-        // Alerts based on response
-        if (jsonRes.message === 'User Account Created') {
-          this.signUpSuccess(
-            'User account has been created successfully. Please login to use CodeRoyale!'
-          );
-        } else if (jsonRes.message === 'User Already Exists') {
-          this.signUpError(
-            'Sorry, email already exists please sign up with a different email!'
-          );
-        } else {
-          this.signUpError('Sorry, couldnt sign up. Please try again!');
-        }
-      })
-      .catch((err) => {
-        // Error response from server
-        this.signUpError('Sorry, couldnt sign up. Please try again later!');
-      });
+  const handleGoogleData = (data) => {
+    setGoogleData(data);
   };
 
-  handleGoogleData = (data) => {
-    this.setState({ googleData: data });
+  useEffect(() => {
     // Send to CodeRoyale API for signing up
-    this.sendToServer();
-  };
-
-  render() {
-    if (this.state.loggedIn) {
-      return <Redirect to='/dashboard' />;
+    if (googleData != null) {
+      setIsLoading(true);
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Origin', CLIENT_URL);
+      headers.append('Access-Control-Allow-Credentials', 'true');
+      // Data to be sent to API
+      const thirdPartyData = {
+        issuer: 'google',
+        signUpType: 'native',
+        idToken: googleData.wc.id_token,
+      };
+      fetch(SIGNUP_API, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(thirdPartyData),
+      })
+        .then((res) => res.json())
+        .then((jsonRes) => {
+          // Success response from server
+          setIsLoading(false);
+          setGoogleData(null);
+          // Alerts based on response
+          if (jsonRes.message === 'User Account Created') {
+            setIsSignedUp(true);
+            signUpSuccess(
+              'User account has been created successfully. Please login to use CodeRoyale!'
+            );
+          } else if (jsonRes.message === 'User Already Exists') {
+            setIsSignedUp(false);
+            signUpError(
+              'Sorry, email already exists please sign up with a different email!'
+            );
+          } else {
+            setIsSignedUp(false);
+            signUpError('Sorry, couldnt sign up. Please try again!');
+          }
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setGoogleData(null);
+          setIsSignedUp(false);
+          // Error response from server
+          signUpError('Sorry, couldnt sign up. Please try again later!');
+        });
     }
-    return (
+  }, [googleData]);
+
+  let content = (
+    <div className='signup-section-container'>
+      <div className='signup-section-content'>
+        <center>
+          <p className='signup-section-title'>Sign up for CodeRoyale</p>
+          <GoogleSignIn
+            text='Sign up with Google'
+            sendGoogleData={handleGoogleData}
+          />
+          <p className='signup-section-sign-up'>
+            Already a member?{' '}
+            <Link to='/signin' style={{ textDecoration: 'none' }}>
+              <span className='span-text'>Sign in now</span>
+            </Link>
+          </p>
+        </center>
+      </div>
+    </div>
+  );
+
+  if (isSignedUp) {
+    return <Redirect to='/dashboard' />;
+  }
+
+  if (isLoading) {
+    content = (
       <div className='signup-section-container'>
         <div className='signup-section-content'>
           <center>
             <p className='signup-section-title'>Sign up for CodeRoyale</p>
             <GoogleSignIn
               text='Sign up with Google'
-              sendGoogleData={this.handleGoogleData}
+              sendGoogleData={handleGoogleData}
             />
             <p className='signup-section-sign-up'>
               Already a member?{' '}
@@ -95,12 +124,13 @@ class SignUpSec extends Component {
                 <span className='span-text'>Sign in now</span>
               </Link>
             </p>
-            <Spin indicator={antIcon} />
+            <Spin indicator={ANT_ICON} />
           </center>
         </div>
       </div>
     );
   }
-}
+  return content;
+};
 
 export default SignUpSec;
