8f33a12686602b8dffb6f8c3d06da19643e231c7
alanhenry0221 <alanhenry0221@gmail.com>
2020-08-15 12:03:07 +0530

    added input and button component to chat

--- src/pages/arenaPage/Chat.js
+++ src/pages/arenaPage/Chat.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ArenaMain.css';
+import { Input } from 'antd';
+import Button from '../../components/button/Button';
 
 function Chat() {
   return (
@@ -15,6 +17,19 @@ function Chat() {
         JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
         JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
         JOEL OP JOEL OP
+      </div>
+      <div className='chat-input'>
+        <Input placeholder='Type here'></Input>
+        <Button
+          type='button'
+          buttonStyle='btn--primary--normal'
+          buttonSize='btn--medium'
+          onClick={() => {
+            console.log('Clicked');
+          }}
+        >
+          SEND
+        </Button>
       </div>
     </div>
   );
