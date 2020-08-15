58c1f37eb721b6f4dda8901b93bcaec2a389e982
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-08-15 12:33:46 +0530

    Added handleChange functions

--- src/pages/arenaPage/Chat.js
+++ src/pages/arenaPage/Chat.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ArenaMain.css';
 import { Input } from 'antd';
 import Button from '../../components/button/Button';
 
 function Chat() {
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleMessageSend = (e) => {
+    console.log(message);
+  };
+
   return (
     <div className='chat-body'>
       <div className='chat-header'>CHAT</div>
       <div className='chat-container'>
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP JOEL OP
-        JOEL OP JOEL OP
+        <p>Messages here</p>
       </div>
       <div className='chat-input'>
-        <Input placeholder='Type here'></Input>
+        <Input onChange={handleMessageChange} placeholder='Type here'></Input>
         <Button
           type='button'
           buttonStyle='btn--primary--normal'
           buttonSize='btn--medium'
-          onClick={() => {
-            console.log('Clicked');
-          }}
+          onClick={handleMessageSend}
         >
           SEND
         </Button>
