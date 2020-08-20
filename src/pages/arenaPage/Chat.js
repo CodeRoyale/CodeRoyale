d7a1ef789d847a8f3ba31d4cb43ad3f5a55733a7
alanhenry0221 <alanhenry0221@gmail.com>
2020-08-20 10:00:57 +0530

    chat msgs now appear in chat box

--- src/pages/arenaPage/Chat.js
+++ src/pages/arenaPage/Chat.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ArenaMain.css';
 import { Input } from 'antd';
+import { v4 as uuidv4 } from 'uuid';
 
 function Chat({ socket }) {
   const [state, setState] = useState({ message: '', name: 'userName' });
+  const [messageList, setList] = useState([]);
 
   const handleMessageChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
+  useEffect(() => {
+    socket.on('RCV_MSG', (data) => {
+      console.log(data);
+      console.log(data.content);
+      const newList = messageList.concat({
+        id: uuidv4(),
+        source: data.userName + ': ',
+        msg: data.content,
+      });
+      setList(newList);
+    });
+  }, [socket, messageList]);
+
   // When message is submitted to server
   const onMessageSubmit = (e) => {
     e.preventDefault();
     const { name, message } = state;
+
+    const newList = messageList.concat({
+      id: uuidv4(),
+      source: 'You: ',
+      msg: message,
+    });
+    setList(newList);
+
     console.log(message);
     console.log(socket);
     socket.emit('SEND_MSG', { content: message }, (data) => {
@@ -25,7 +48,14 @@ function Chat({ socket }) {
     <div className='chat-body'>
       <div className='chat-header'>CHAT</div>
       <div className='chat-container'>
-        Messages sent here are displayed here
+        <ul>
+          {messageList.map((item) => (
+            <li key={item.id}>
+              {item.source}
+              {item.msg}
+            </li>
+          ))}
+        </ul>
       </div>
       <form onSubmit={onMessageSubmit}>
         <div className='chat-input'>
