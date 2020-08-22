539192311edd05a657a43d76d8561358fed86c9e
sanithps98 <pssanith@karunya.edu.in>
2020-08-22 20:38:00 +0530

    Completed getQuestions

--- server/utils/qapiConn.js
+++ server/utils/qapiConn.js
@@ -1,6 +1,25 @@
-const getQuestions = async (n) => {
-  // return n random questions
-  return [{ q: "awd aw " }, { q: "vawd aw " }, { q: "awasdawd aw z" }];
+const fetch = require('node-fetch');
+
+const getQuestions = async () => {
+
+Url = 'http://localhost:3000/questions/random?noIds=3';
+
+async function getQuestions(url = '') {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      useQueryString: true,
+      accept: 'application/json',
+    },
+  });
+  return await response.json();
+}
+// (async () => {
+//   let data = await getQuestions(Url);
+//   return data;
+// })();
+  
 };
 
 module.exports = { getQuestions };
