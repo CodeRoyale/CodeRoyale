2965b219b59ea2542db3c73ec7c3c2ed41399155
Sawarni Swaroop <sawarni99@gmail.com>
2020-08-27 13:24:47 +0530

    Added rsuite with custom theme

--- src/pages/testPage/TestPage.js
+++ src/pages/testPage/TestPage.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Select } from 'antd';
-
-const { Option } = Select;
-
-function handleChange(value) {
-  console.log(`selected ${value}`);
-}
+import 'rsuite/lib/styles/index.less';
+import { Alert } from 'rsuite';
 
 export default function SpringPopper() {
   return (
     <div>
-      <Select
-        defaultValue='lucy'
-        style={{ width: 120 }}
-        onChange={handleChange}
-      >
-        <Option value='jack'>Jack</Option>
-        <Option value='lucy'>Lucy</Option>
-        <Option value='disabled' disabled>
-          Disabled
-        </Option>
-        <Option value='Yiminghe'>yiminghe</Option>
-      </Select>
+      <button onClick={() => Alert.error('This is an error message.')}>
+        {' '}
+        Error{' '}
+      </button>
     </div>
   );
 }
