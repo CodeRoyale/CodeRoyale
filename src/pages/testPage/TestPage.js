a3028f4e8c5a50f79692fe395d5c1b2c02df4407
Sawarni Swaroop <sawarni99@gmail.com>
2020-08-27 13:39:24 +0530

    Added testPgae

--- src/pages/testPage/TestPage.js
+++ src/pages/testPage/TestPage.js
@@ -1,16 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import 'rsuite/lib/styles/index.less';
-import { Alert } from 'rsuite';
-
-export default function SpringPopper() {
-  return (
-    <div>
-      <button onClick={() => Alert.error('This is an error message.')}>
-        {' '}
-        Error{' '}
-      </button>
-=======
 import './TestPage.css';
 
 export default function SpringPopper() {
@@ -21,7 +9,6 @@ export default function SpringPopper() {
         <span class='icon'></span>
         <span class='buttonText'>Google</span>
       </div>
->>>>>>> origin/develop
     </div>
   );
 }
