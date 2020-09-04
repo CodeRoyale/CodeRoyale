46c7b72afabca89a5f68531b42402b3385a7c223
sanithps98 <pssanith@karunya.edu.in>
2020-09-04 11:35:48 +0530

    Added ROOM_CLOSED Server Action

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-const { encryptData } = require('../utils/auth');
-const { setRoom, getUser, setTeam, mapNameToId } = require('./userController');
-const { getQuestions } = require('../utils/qapiConn');
-=======
 const { encryptData } = require("../utils/auth");
 const { setRoom, getUser, setTeam, mapNameToId } = require("./userController");
 const { getQuestions } = require("../utils/qapiConn");
 const { ROOM_DEFAULTS, ROOM_LIMITS } = require("./config");
 
 console.log(ROOM_DEFAULTS, ROOM_LIMITS);
->>>>>>> 8c9d2508b2f1f396485c7242c802b17b4b0ce324
 const {
   ROOM_UPDATED,
   RCV_MSG,
@@ -24,10 +18,10 @@ const {
   COMPETITION_STARTED,
   COMPETITION_STOPPED,
   ROOM_CLOSED,
-} = require('../socketActions/serverActions');
+} = require("../socketActions/serverActions");
 
-const { io } = require('../server');
-const { CLOSE_ROOM } = require('../socketActions/userActions');
+const { io } = require("../server");
+const { CLOSE_ROOM } = require("../socketActions/userActions");
 
 // this is my db for now
 rooms = {};
@@ -126,7 +120,7 @@ const joinRoom = ({ userName, room_id, team_name }, { socket }) => {
       let user = getUser(userName);
       if (user.room_id) {
         // already in a grp dont allow
-        throw new Error('User already in room');
+        throw new Error("User already in room");
       }
 
       // succesfull (user will now be added)
@@ -147,7 +141,7 @@ const joinRoom = ({ userName, room_id, team_name }, { socket }) => {
         });
       } else {
         // else bench the user
-        team_name = '';
+        team_name = "";
         rooms[room_id].state.bench.push(userName);
       }
 
@@ -162,7 +156,7 @@ const joinRoom = ({ userName, room_id, team_name }, { socket }) => {
         type: JOINED_ROOM,
         data: { userName },
       });
-      console.log(userName, ' joined from ', room_id);
+      console.log(userName, " joined from ", room_id);
       return rooms[room_id];
     }
     return false;
@@ -208,8 +202,8 @@ const removeUserFromRoom = ({ userName }) => {
     rooms[room_id].state.cur_memCount -= 1;
 
     // tell others
-    console.log(userName, ' removed from ', room_id);
-    setRoom(userName, '');
+    console.log(userName, " removed from ", room_id);
+    setRoom(userName, "");
     socket.to(room_id).emit(ROOM_UPDATED, {
       type: LEFT_ROOM,
       data: { userName },
@@ -230,7 +224,7 @@ const createTeam = ({ userName, team_name }, { socket }) => {
     const { room_id } = getUser(userName);
     // if user not in room or not admin of the room
     if (!room_id || rooms[room_id].config.admin !== userName) {
-      throw new Error('Only admin can do this');
+      throw new Error("Only admin can do this");
     }
 
     if (
@@ -266,7 +260,7 @@ const joinTeam = ({ userName, team_name }, { socket }) => {
     ) {
       if (user.team_name) {
         //ditch prev team
-        throw new Error('Already in a team');
+        throw new Error("Already in a team");
       }
 
       // remove from bench
@@ -305,7 +299,7 @@ const leaveTeam = ({ userName }, { socket }) => {
       );
       rooms[room_id].teams[team_name] = newTeam;
       rooms[room_id].state.bench.push(userName);
-      setTeam(userName, '');
+      setTeam(userName, "");
 
       // tell eveyone
       socket.leave(`${room_id}/${team_name}`);
@@ -339,11 +333,11 @@ const closeRoom = ({ userName }, { socket }) => {
       allMembers.forEach((userName) => {
         // this is a server action notify all
         // TODO --> add kick all and remove functions for sockets
-        setRoom(userName, '');
+        setRoom(userName, "");
       });
 
       // delete the stupid room
-      const dataToEmit = 'Room Closed';
+      const dataToEmit = "Room Closed";
       socket.to(room_id).emit(ROOM_CLOSED, {
         data: { dataToEmit },
       });
@@ -423,7 +417,7 @@ const registerVotes = ({ userName, votes }, { socket }) => {
     // veto should be on
     // should not have already voted
     if (!team_name || !vetoOn || voted.includes(userName)) {
-      throw new Error('Not in a team or voting stopped or already voted');
+      throw new Error("Not in a team or voting stopped or already voted");
     }
 
     // valid votes only
@@ -451,7 +445,7 @@ const doVeto = async (quesIds, room_id, count, socket) => {
         rooms[room_id].competition.contestOn ||
         rooms[room_id].competition.veto.vetoOn
       ) {
-        throw new Error('Veto not allowed now');
+        throw new Error("Veto not allowed now");
       }
 
       // set the room state
@@ -512,10 +506,10 @@ const startCompetition = async ({ userName }, { socket }) => {
       room.competition.contestOn ||
       room.competition.veto.vetoOn
     ) {
-      throw new Error('Room does not meet requirements');
+      throw new Error("Room does not meet requirements");
     }
 
-    console.log('Starting competition', userName);
+    console.log("Starting competition", userName);
     // start veto now and wait for it to end
     const allQuestions = await getQuestions(room.competition.veto.quesCount);
     await doVeto(allQuestions, room_id, room.competition.max_questions, socket);
--- server/socketActions/serverActions.js
+++ server/socketActions/serverActions.js
@@ -1,44 +1,44 @@
 // connection ack
-const CONNECTION_ACK = 'CONNECTION_ACK';
+const CONNECTION_ACK = "CONNECTION_ACK";
 
 // connection deny
-const CONNECTION_DENY = 'CONNECTION_DENY';
+const CONNECTION_DENY = "CONNECTION_DENY";
 
 // update client about updated room
 // currently this will only track members updation
-const ROOM_UPDATED = 'ROOM_UPDATED';
+const ROOM_UPDATED = "ROOM_UPDATED";
 
 // types of ROOM_UPDATED
 // 1. a user joined the room
-const JOINED_ROOM = 'JOINED_ROOM';
+const JOINED_ROOM = "JOINED_ROOM";
 // 2. joined a team in the room
-const JOINED_TEAM = 'JOINED_TEAM';
+const JOINED_TEAM = "JOINED_TEAM";
 // 3 . left the team
-const LEFT_TEAM = 'LEFT_TEAM';
+const LEFT_TEAM = "LEFT_TEAM";
 // 4. left the room
-const LEFT_ROOM = 'LEFT_ROOM';
+const LEFT_ROOM = "LEFT_ROOM";
 // 5. create a team
-const TEAM_CREATED = 'TEAM_CREATED';
+const TEAM_CREATED = "TEAM_CREATED";
 
 // 6. added private member
 const ADDED_PRIVATE_MEMBER = "ADDED_PRIVATE_MEMBER";
 
 // to forward sent message
-const RCV_MSG = 'RCV_MSG';
+const RCV_MSG = "RCV_MSG";
 
 // veto has started
-const VETO_START = 'VETO_START';
+const VETO_START = "VETO_START";
 // veto has stopeed
-const VETO_STOP = 'VETO_STOP';
+const VETO_STOP = "VETO_STOP";
 
 // competiion has started
-const COMPETITION_STARTED = 'COMPETITION_STARTED';
+const COMPETITION_STARTED = "COMPETITION_STARTED";
 
 // competiion has started
-const COMPETITION_STOPPED = 'COMPETITION_STOPPED';
+const COMPETITION_STOPPED = "COMPETITION_STOPPED";
 
 // close room
-const ROOM_CLOSED = 'ROOM_CLOSED';
+const ROOM_CLOSED = "ROOM_CLOSED";
 
 module.exports = {
   CONNECTION_ACK,
