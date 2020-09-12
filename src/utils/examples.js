7607a702fc80eee5e1bb51f054b4f10a6e1107a3
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-09-13 02:22:42 +0530

    Added profileData from user api instead of static data

--- src/utils/examples.js
+++ src/utils/examples.js
@@ -1,7 +1,9 @@
+let userApiData = localStorage.getItem('user-data');
+userApiData = JSON.parse(userApiData);
+
 let profileData = {
-  imageUrl:
-    'https://media-exp1.licdn.com/dms/image/C5103AQHuIxezqseoGQ/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=RWNCY_VThJLkPpUN9-529erEFlZREIhqlMLGDB8bBSU',
-  username: Date.now(),
+  imageUrl: userApiData.picture,
+  username: userApiData.userName,
   email: 'sawarni99@gmail.com',
 };
 export default profileData;
