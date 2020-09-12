c4c579cebfe64a0109093d70972354965fe40105
Sawarni Swaroop <sawarni99@gmail.com>
2020-09-12 11:00:29 +0530

    Created a simple page for winLose

--- src/pages/winLosePage/WinLoseMain.js
+++ src/pages/winLosePage/WinLoseMain.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WinLoseMain.css';
 import { WON, LOST } from '../../utils/constants';
+import { Icon, Whisper, Tooltip } from 'rsuite';
+import { Redirect } from 'react-router';
 
-function WinLoseMain() {
-  const action = WON;
-  const greenRGB = '60, 255, 0, 0.5';
-  const redRGB = '255, 0, 17, 0.5';
+function WinLoseMain({ action }) {
+  const [backClicked, setBackClicked] = useState(false);
+  const greenRGB = '60, 255, 0';
+  const redRGB = '255, 0, 17';
+  const alpha = ',0.7';
   let rgb = null;
+
+  if (backClicked) {
+    return <Redirect to='/dashboard' />;
+  }
+
   if (action === WON) {
     rgb = greenRGB;
   } else if (action === LOST) {
     rgb = redRGB;
   }
-  console.log(rgb);
   return (
     <div className='win-lose'>
-      <div>
-        <img
-          style={{ height: '100vh', width: '100%' }}
-          src='/images/results.svg'
-          alt='n/a'
-        />
+      <div className='win-lose-back'>
+        <Whisper
+          placement='right'
+          trigger='hover'
+          speaker={<Tooltip>Go to Dashboard</Tooltip>}
+        >
+          <Icon
+            onClick={() => setBackClicked(true)}
+            icon='back-arrow'
+            size='2x'
+            style={{ margin: '20px' }}
+          />
+        </Whisper>
       </div>
+
       <div className='win-lose-result-container'>
-        <div className='win-lose-result'>
+        <div
+          style={{
+            background: 'rgba(' + rgb + alpha + ')',
+            border: 'solid rgb(' + rgb + ') 5px',
+          }}
+          className='win-lose-result'
+        >
           <b>{action}</b>
         </div>
       </div>
