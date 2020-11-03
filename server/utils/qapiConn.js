3f3049adfa008033e1385f7e90706bb596e7f75d
Naveen <naveensreevalsan@gmail.com>
2020-11-03 16:42:51 +0530

    Tokenized Qapi request

--- server/utils/qapiConn.js
+++ server/utils/qapiConn.js
@@ -1,11 +1,13 @@
 const fetch = require("node-fetch");
 
 const getQuestions = async (noIds) => {
-  let response = await fetch(`${process.env.QAPI_URL}random?noIds=${noIds}`, {
+  let response = await fetch(`${process.env.QAPI_URL}random?noIds=${noIds}?lobbyID=${process.env.LOBBY_ID}`, 
+  {
     method: "GET",
     headers: {
       useQueryString: true,
       accept: "application/json",
+      Authorization : `Bearer ${process.env.QAPI_BEARER}`
     },
   });
   response = await response.json();
@@ -14,12 +16,13 @@ const getQuestions = async (noIds) => {
 
 const getTestcase = async (id) => {
   let response = await fetch(
-    `${process.env.QAPI_URL + "getTestcase"}?id=${id}`,
+    `${process.env.QAPI_URL + "getTestcase"}?id=${id}?lobbyID=${process.env.LOBBY_ID}`,
     {
       method: "GET",
       headers: {
         useQueryString: true,
         accept: "application/json",
+        Authorization : `Bearer ${process.env.QAPI_BEARER}`
       },
     }
   );
