3bd060b3e00efcdec83b7ed3963e0db2f63f8f79
Sawarni Swaroop <sawarni99@gmail.com>
2020-11-05 13:02:57 +0530

    Initial Arena Page

--- src/pages/arenaPage/ArenaMain.js
+++ src/pages/arenaPage/ArenaMain.js
@@ -1,78 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import NavBar from '../../components/navBar/NavBar';
+import React from 'react';
 import './ArenaMain.css';
-import Problem from './Problem';
-import Chat from './Chat';
-import Solution from './Solution';
-import { Redirect } from 'react-router';
-import { Loader } from 'rsuite';
-import { connect } from 'react-redux';
-import { getQuestion } from '../../actions/arenaActions';
+import ProfileButton from '../../components/profileButton/ProfileButton';
+import profileData from '../../utils/examples';
 
-const ArenaMain = ({ socketData, arenaData, vetoData, getQuestion }) => {
-  const socket = socketData.socket;
-  const [currentQuestion, setCurrentQuestion] = useState();
+const chat_icon = 'chat-arena.svg';
+const question_icon = 'problem.svg';
 
-  useEffect(() => {
-    if (vetoData.contestQuestionIDs !== null) {
-      getQuestion(vetoData.contestQuestionIDs);
-    }
-  }, [vetoData.contestQuestionIDs, getQuestion]);
-
-  if (socket === null) {
-    return <Redirect to='/lobby' />;
-  }
-
-  const handleGetCurrQuestion = (data) => {
-    setCurrentQuestion(data);
-  };
-
-  let content = (
-    <div className='arena-page'>
-      <div>
-        <NavBar loggedIn={true} />
-      </div>
-
-      <div className='arena-body'>
-        <div className='left-container'>
-          <Problem
-            questions={arenaData.questions}
-            getCurrentQuestion={handleGetCurrQuestion}
-          />
-          <Chat socket={socket} />
-        </div>
-
-        <div className='right-container'>
-          <Solution
-            socket={socket}
-            questions={arenaData.questions}
-            currentQuestion={currentQuestion}
-          />
-        </div>
-      </div>
+const Header = ({name, icon}) =>{
+  const icon_path = '/images/' + icon;
+  return (
+    <div className='arena-side-bar-header'>    
+      <img style={{height:'25px', width:'25px'}} src={icon_path} alt='null' />
+      <b><p style={{fontSize: '16px', marginLeft: '4px'}}>{name}</p></b>
     </div>
-  );
+  )
+}
 
-  if (arenaData.isLoading) {
-    content = (
-      <div className='arena-page'>
-        <div>
-          <NavBar />
-        </div>
-        <div className='arena-page-loading'>
-          <Loader size='md' content='Setting up your coding environment...' />
+const ArenaMain = () =>{
+  return (
+    <div className='arena'>
+      <div className='arena-body'>
+        <div className='arena-left'></div>
+        <div className='arena-right'>
+          <div>
+            <div className='arena-profile'>
+              <ProfileButton profileData={profileData}/>
+              <p className='arena-profile-name'>Sawarni Swaroop</p>
+            </div>
+            <div className='arena-divider'/>
+          </div>
+          <Header name='Question' icon={question_icon}/>          
+          <div className='arena-question-status'>
+
+          </div>
+          <Header name='Chat' icon={chat_icon}/>
         </div>
       </div>
-    );
-  }
+      
+    </div>
+  )
+}
+export default ArenaMain;
 
-  return content;
-};
 
-const mapStateToProps = (state) => ({
-  socketData: state.socketData,
-  vetoData: state.vetoData,
-  arenaData: state.arenaData,
-});
+// import React, { useState, useEffect } from 'react';
+// import NavBar from '../../components/navBar/NavBar';
+// import './ArenaMain.css';
+// import Problem from './Problem';
+// import Chat from './Chat';
+// import Solution from './Solution';
+// import { Redirect } from 'react-router';
+// import { Loader } from 'rsuite';
+// import { connect } from 'react-redux';
+// import { getQuestion } from '../../actions/arenaActions';
 
-export default connect(mapStateToProps, { getQuestion })(ArenaMain);
+// const ArenaMain = ({ socketData, arenaData, vetoData, getQuestion }) => {
+//   const socket = socketData.socket;
+//   const [currentQuestion, setCurrentQuestion] = useState();
+
+//   useEffect(() => {
+//     if (vetoData.contestQuestionIDs !== null) {
+//       getQuestion(vetoData.contestQuestionIDs);
+//     }
+//   }, [vetoData.contestQuestionIDs, getQuestion]);
+
+//   if (socket === null) {
+//     return <Redirect to='/lobby' />;
+//   }
+
+//   const handleGetCurrQuestion = (data) => {
+//     setCurrentQuestion(data);
+//   };
+
+//   let content = (
+//     <div className='arena-page'>
+//       <div>
+//         <NavBar loggedIn={true} />
+//       </div>
+
+//       <div className='arena-body'>
+//         <div className='left-container'>
+//           <Problem
+//             questions={arenaData.questions}
+//             getCurrentQuestion={handleGetCurrQuestion}
+//           />
+//           <Chat socket={socket} />
+//         </div>
+
+//         <div className='right-container'>
+//           <Solution
+//             socket={socket}
+//             questions={arenaData.questions}
+//             currentQuestion={currentQuestion}
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+
+//   if (arenaData.isLoading) {
+//     content = (
+//       <div className='arena-page'>
+//         <div>
+//           <NavBar />
+//         </div>
+//         <div className='arena-page-loading'>
+//           <Loader size='md' content='Setting up your coding environment...' />
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   return content;
+// };
+
+// const mapStateToProps = (state) => ({
+//   socketData: state.socketData,
+//   vetoData: state.vetoData,
+//   arenaData: state.arenaData,
+// });
+
+// export default connect(mapStateToProps, { getQuestion })(ArenaMain);
--- src/utils/examples.js
+++ src/utils/examples.js
@@ -20,7 +20,7 @@
 
 // For now...
 const profileData = {
-  imageUrl:
+  picture:
     'https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
   username: Date.now(),
   email: 'sawarni99@gmail.com',
