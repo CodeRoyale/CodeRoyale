acec6d56f59d54ac52a6222fcbd7d3d3efd4b649
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2020-11-13 15:44:43 +0530

    Added fallback token

--- src/pages/testPage/TestPage.js
+++ src/pages/testPage/TestPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ScoreboardTeam from '../../pages/scoreboard/ScoreboardTeam';
+import jwt from 'jsonwebtoken';
 import './TestPage.css';
 
 const TestPage = () => {
   // document.body.style = 'background: gray;';
+  console.log(jwt.decode(process.env.REACT_FALLBACK_TOKEN));
   return (
     <div className='test-page'>
       <ScoreboardTeam />
