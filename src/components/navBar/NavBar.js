39e3d5664e919e052fe2ca7422bdbf7afc792e89
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-09 16:31:51 +0530

    Refactored navBar (loggedin)

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -4,21 +4,23 @@ import LogoContainer from '../../components/logoContainer/LogoContainer';
 import ProfileButton from '../../components/profileButton/ProfileButton';
 import profileData from '../../utils/profileData';
 import { Flex, Button, Spacer } from '@chakra-ui/react';
-import './NavBar.css';
 
 const NavBar = ({ loggedIn }) => {
   const history = useHistory();
 
   // Default navBar (loggedIn)
   let content = (
-    <div className='nav-bar'>
-      <div className='nav-bar-logo'>
-        <LogoContainer />
-      </div>
-      <div className='nav-bar-profile'>
-        <ProfileButton profileData={profileData()} />
-      </div>
-    </div>
+    <Flex
+      as='nav'
+      height='9vh'
+      alignItems='center'
+      padding='1em'
+      boxShadow='0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(250, 250, 242)'
+    >
+      <LogoContainer />
+      <Spacer />
+      <ProfileButton profileData={profileData()} />
+    </Flex>
   );
 
   // If user is not loggedIn
