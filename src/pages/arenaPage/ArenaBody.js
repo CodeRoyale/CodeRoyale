d7a2f9919ca37003ac7fbf6f94d9789d413478a4
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-17 16:07:42 +0530

    Refactored arena problem

--- src/pages/arenaPage/ArenaBody.js
+++ src/pages/arenaPage/ArenaBody.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { Flex } from '@chakra-ui/react';
 import ArenaSolution from './ArenaSolution';
+import ArenaProblem from './ArenaProblem';
 
 const ArenaBody = () => {
+  const sampleQuestion = {
+    tags: [],
+    timeLimit: 1,
+    sourceLimit: 256,
+    _id: '5f5c8eba5ca382001784b190',
+    questionTitle: 'Payment Without Change',
+    problemCode: 'PMWC',
+    description:
+      'You have a coins of value n and b coins of value 1. You always pay in exact change, so you want to know if there exist such x and y that if you take x (0≤x≤a) coins of value n and y (0≤y≤b) coins of value 1, then the total value of taken coins will be S.\n\nYou have to answer q\nindependent test cases.',
+    format:
+      'Input\n\nThe first line of the input contains one integer q\n(1≤q≤104) — the number of test cases. Then q\n\ntest cases follow.\n\nThe only line of the test case contains four integers a\n, b, n and S (1≤a,b,n,S≤109) — the number of coins of value n, the number of coins of value 1, the value n\n\nand the required total value.\nOutput\n\nFor the i\n-th test case print the answer on it — YES (without quotes) if there exist such x and y that if you take x coins of value n and y coins of value 1, then the total value of taken coins will be S\n\n, and NO otherwise.\n\nYou may print every letter in any case you want (so, for example, the strings yEs, yes, Yes and YES will all be recognized as positive answer).',
+    io: [
+      {
+        input: '4\n1 2 3 4\n1 2 3 6\n5 2 6 27\n3 3 5 18\n',
+        output: 'YES\nNO\nNO\nYES',
+      },
+      {
+        input: '4\n1 2 3 4\n1 2 3 6\n5 2 6 27\n3 3 5 18\n',
+        output: 'YES\nNO\nNO\nYES',
+        explanation: 'Have prefect coins',
+      },
+    ],
+    author: 'Nangaa',
+    dateAdded: '2020-09-12',
+    difficulty: 1,
+    __v: 0,
+  };
+
   return (
     <Flex
       pos='absolute'
@@ -13,6 +42,7 @@ const ArenaBody = () => {
       flexDir='column'
       padding='1em'
     >
+      <ArenaProblem currentQuestion={sampleQuestion} />
       <ArenaSolution />
     </Flex>
   );
--- src/pages/arenaPage/ArenaProblem.js
+++ src/pages/arenaPage/ArenaProblem.js
@@ -1,70 +1,76 @@
 import React from 'react';
-import './ArenaMain.css';
+import { Flex, Stack, Text } from '@chakra-ui/react';
 
-const ArenaProblem = ({currentQuestion}) => {
-  let questionIO = [];
-  let questionTitle = null;
-  let questionCode = null;
-  let questionDescription = null;
-  let questionFormat = null;
+const ArenaProblem = ({ currentQuestion }) => {
+  const {
+    questionTitle,
+    problemCode: questionCode,
+    format: questionFormat,
+    description: questionDescription,
+    io: questionIO,
+  } = currentQuestion;
 
-  if (currentQuestion !== null && currentQuestion.io !== undefined) {
-    questionTitle = currentQuestion.questionTitle;
-    questionCode = currentQuestion.problemCode;
-    questionFormat = currentQuestion.format;
-    questionDescription = currentQuestion.description;
-    questionIO = currentQuestion.io;
-    for(let i=0; i<questionIO.length; i++){
-      questionIO[i]['key'] = i; 
-    }
-  }
-  
-  const questionIOView = questionIO.map((data) => (
-    <div key={data.key}>
-      <div>
-        <div className='problem-heading'>
-          <b>Input</b>
-        </div>
-        {data.input}
-        <div className='problem-heading'>
-          <b>Output</b>
-        </div>
-        {data.output}
-        <div className='problem-heading'>
-          <b>Explanation</b>
-        </div>
-        {data.explanation}
-      </div>
-      <br />
-    </div>
+  const questionInputOutput = questionIO.map((item, index) => (
+    <>
+      <Stack marginTop='1em'>
+        <Text fontSize='lg' fontWeight='bold'>
+          Input
+        </Text>
+        <Text fontSize='md' whiteSpace='pre-line'>
+          {item.input}
+        </Text>
+      </Stack>
+      <Stack marginTop='1em'>
+        <Text fontSize='lg' fontWeight='bold'>
+          Output
+        </Text>
+        <Text fontSize='md' whiteSpace='pre-line'>
+          {item.output}
+        </Text>
+      </Stack>
+      <Stack marginTop='1em'>
+        <Text fontSize='lg' fontWeight='bold'>
+          Explanation
+        </Text>
+        <Text fontSize='md' whiteSpace='pre-line'>
+          {item.explanation ? item.explanation : '(No explanation)'}
+        </Text>
+      </Stack>
+    </>
   ));
 
   return (
-    <div className='problem'>
-      <div className='problem-title'>
-        <b>{questionTitle}</b>
-      </div>
-      <div className='problem-code'>{questionCode}</div>
-      <br />
-
-      <div className='problem-heading'>
-        <b>Description</b>
-      </div>
-      <div>
-        <p>{questionDescription}</p>
-      </div>
-      <br />
-
-      <div className='problem-heading'>
-        <b>Format</b>
-      </div>
-      <div>
-        <p>{questionFormat}</p>
-      </div>
-      <br />
-
-      {questionIOView}
-    </div>
+    <Flex
+      as='div'
+      flexDir='column'
+      bgColor='white'
+      marginBottom='2em'
+      padding='2em'
+    >
+      <Stack>
+        <Text fontWeight='bold' fontSize='3xl'>
+          {questionTitle}
+        </Text>
+        <Text fontSize='xl'>{questionCode}</Text>
+      </Stack>
+      <Stack marginTop='1em'>
+        <Text fontSize='lg' fontWeight='bold' whiteSpace='pre-line'>
+          Description
+        </Text>
+        <Text fontSize='md' whiteSpace='pre-line'>
+          {questionDescription}
+        </Text>
+      </Stack>
+      <Stack marginTop='1em'>
+        <Text fontSize='lg' fontWeight='bold'>
+          Format
+        </Text>
+        <Text fontSize='md' whiteSpace='pre-line'>
+          {questionFormat}
+        </Text>
+      </Stack>
+      {questionInputOutput}
+    </Flex>
   );
 };
 
