4d0ff04128c8113b10940d57e6022ef93dd9006c
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-22 11:42:24 +0530

    Refactored socketActions

--- src/actions/socketActions.js
+++ src/actions/socketActions.js
@@ -12,93 +12,80 @@ import {
 } from './types';
 import io from 'socket.io-client';
 import { ROOM_CLOSED } from '../utils/constants';
-const ENDPOINT = process.env.REACT_APP_LOBBY_SERVER;
+const LOBBY_SERVER = process.env.REACT_APP_LOBBY_SERVER;
 
-const requestSocketConnection = () => {
-  return {
+// Connection to socket server
+export const connectSocket = () => (dispatch) => {
+  dispatch({
     type: SOCKET_LOADING,
-  };
-};
-const socketConnectionSuccess = (socket) => {
-  return {
-    type: SOCKET_SUCCESS,
-    payload: socket,
-  };
-};
-
-const socketConnectionFailure = (error) => {
-  return {
-    type: SOCKET_FAIL,
-    payload: error,
-  };
-};
-
-// Async Action to connect to socket...
-export const connectSocket = () => {
-  return (dispatch) => {
-    dispatch(requestSocketConnection());
-    const options = {
-      transportOptions: {
-        polling: {
-          extraHeaders: {
-            Authorization: `Bearer ${localStorage.token}`,
-          },
+  });
+  const options = {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: `Bearer ${localStorage.token}`,
         },
       },
-    };
-    let socket = io.connect(ENDPOINT, options);
-    socket.on(CONNECTION_ACK, () => {
-      console.log(CONNECTION_ACK);
-      dispatch(socketConnectionSuccess(socket));
-    });
-    socket.on(CONNECTION_DENY, () => {
-      console.log(CONNECTION_DENY);
-      dispatch(socketConnectionFailure(CONNECTION_DENY));
-    });
-
-    // On Message receive...
-    socket.on('RCV_MSG', (data) => {
-      if (data !== null && data.content !== undefined) {
-        if (data.toTeam) {
-          dispatch({
-            type: CHAT_TEAM_SUCCESS,
-            payload: {
-              message: data.content,
-              source: data.userName,
-            },
-          });
-        } else {
-          dispatch({
-            type: CHAT_EVERYONE_SUCCESS,
-            payload: {
-              message: data.content,
-              source: data.userName,
-            },
-          });
-        }
-      } else {
-        dispatch({
-          type: CHAT_FAIL,
-          payload: 'No chat Came',
-        });
-      }
-      console.log('chat data', data);
-    });
-
-    // On Close room
-    socket.on('ROOM_CLOSED', (data) => {
-      if (data !== null) {
-        dispatch({
-          type: ROOM_SUCCESS,
-          payload: null,
-          action: ROOM_CLOSED,
-        });
-      } else {
-        dispatch({
-          type: ROOM_FAIL,
-          payload: data,
-        });
-      }
-    });
+    },
   };
+  let socket = io.connect(LOBBY_SERVER, options);
+  socket.on(CONNECTION_ACK, () => {
+    console.log(CONNECTION_ACK);
+    dispatch({
+      type: SOCKET_SUCCESS,
+      payload: socket,
+    });
+  });
+  socket.on(CONNECTION_DENY, () => {
+    console.log(CONNECTION_DENY);
+    dispatch({
+      type: SOCKET_FAIL,
+      payload: CONNECTION_DENY,
+    });
+  });
+
+  // On Message receive...
+  socket.on('RCV_MSG', (data) => {
+    if (data !== null && data.content !== undefined) {
+      if (data.toTeam) {
+        dispatch({
+          type: CHAT_TEAM_SUCCESS,
+          payload: {
+            message: data.content,
+            source: data.userName,
+          },
+        });
+      } else {
+        dispatch({
+          type: CHAT_EVERYONE_SUCCESS,
+          payload: {
+            message: data.content,
+            source: data.userName,
+          },
+        });
+      }
+    } else {
+      dispatch({
+        type: CHAT_FAIL,
+        payload: 'No chat Came',
+      });
+    }
+    console.log('chat data', data);
+  });
+
+  // On Close room
+  socket.on('ROOM_CLOSED', (data) => {
+    if (data !== null) {
+      dispatch({
+        type: ROOM_SUCCESS,
+        payload: null,
+        action: ROOM_CLOSED,
+      });
+    } else {
+      dispatch({
+        type: ROOM_FAIL,
+        payload: data,
+      });
+    }
+  });
 };
