6a3194326bd33543a400785c5e89ef49a0e878a1
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-05-06 13:08:38 +0530

    Added close room, get room, listeners for room closed and room updated

--- src/service/roomSocket.js
+++ src/service/roomSocket.js
@@ -60,3 +60,54 @@ export const joinRoom = (socket, { room_id }, cb) => {
     return cb('No response from server', null);
   });
 };
+
+export const closeRoom = (socket, cb) => {
+  if (!socket) return false;
+
+  socket.emit('CLOSE_ROOM', {}, (data) => {
+    if (data) {
+      if (data !== ERROR_MSG) {
+        // Close room success
+        return cb(null, data);
+      }
+      // Close room fail
+      return cb(data, null);
+    }
+    // Server didnt send back data
+    return cb('No response from server', null);
+  });
+};
+
+export const getRoom = (socket, { room_id }, cb) => {
+  if (!socket) return false;
+
+  socket.emit('GET_ROOM', { room_id }, (data) => {
+    if (data) {
+      if (data !== ERROR_MSG) {
+        // Get room success
+        return cb(null, data);
+      }
+      // Get room fail
+      return cb(data, null);
+    }
+    return cb('No response from server', null);
+  });
+};
+
+export const roomClosed = (socket, cb) => {
+  if (!socket) return false;
+
+  socket.off('ROOM_CLOSED').on('ROOM_CLOSED', (data) => {
+    console.log(data);
+  });
+};
+
+export const roomUpdated = (socket, cb) => {
+  if (!socket) return false;
+
+  socket.on('ROOM_UPDATED', (data) => {
+    if (data && data.type !== undefined) {
+      console.log(data);
+    }
+  });
+};
