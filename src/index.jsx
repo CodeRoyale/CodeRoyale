37dda82814c1be378d03c6088d78a6872cb1c448
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-05-12 11:38:52 +0530

    Fixed linting in src/index

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { theme } from './theme';
+import theme from './theme';
 import './index.scss';
 
 ReactDOM.render(
