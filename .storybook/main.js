76f5f4caa5c53be74644f8b1452ab0596013feb7
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-02-11 10:02:34 +0530

    Configured storybook for tailwindcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
   stories: [
+    // Paths to the story files
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    });
+
+    return config;
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,4 @@ import '../styles/globals.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
 };
