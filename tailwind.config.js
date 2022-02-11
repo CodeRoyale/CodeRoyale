95bca21b4f0543d25f99e49f77c3adddb1cd3c44
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-02-11 10:31:14 +0530

    Setup colors in tailwind to match CodeRoyale theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      primary: {
+        100: 'var(--color-primary-100)',
+        200: 'var(--color-primary-200)',
+        300: 'var(--color-primary-300)',
+        600: 'var(--color-primary-600)',
+        700: 'var(--color-primary-700)',
+        800: 'var(--color-primary-800)',
+        900: 'var(--color-primary-900)',
+      },
+      buttonPrimary: {
+        default: 'var(--color-button-primary)',
+        hover: 'var(--color-button-primary-hover)',
+        disabled: 'var(--color-button-primary-disabled)',
+      },
+    },
   },
   plugins: [],
 };
