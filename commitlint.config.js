bf71551b310e1f8b2b559986a01d13ef6c01e829
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-05-05 12:50:03 +0530

    chore: added and setup commitizen with commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {},
+};
