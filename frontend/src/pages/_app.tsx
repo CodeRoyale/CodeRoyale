96bcafc50ca178d1cbe01f59dbdef9448564029d
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-05-24 13:26:25 +0530

    feat(frontend): added nprogress for progress bar when loading pages

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import ReactModal from 'react-modal';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import 'nprogress/nprogress.css';
 import '../styles/globals.css';
 
 ReactModal.setAppElement('#__next');
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
