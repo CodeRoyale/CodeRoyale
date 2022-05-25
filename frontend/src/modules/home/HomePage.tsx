da115a8b08b685096f2249b7f81de8cc80354a51
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-05-25 16:48:56 +0530

    feat(frontend): showing home page even after logged in fixed

--- frontend/src/modules/home/HomePage.tsx
+++ frontend/src/modules/home/HomePage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { useRouter } from 'next/router';
 import { googleClientId } from '../../utils/constants';
+import { useMeQuery } from '../../generated/graphql';
 import { AuthFormController } from './AuthFormController';
 import {
   GoogleAuthButtonController,
@@ -8,36 +10,52 @@ import {
 } from './GoogleAuthButtonController';
 
 export const HomePage = () => {
+  const router = useRouter();
+  const { data, loading } = useMeQuery();
+
   const [showAuthForm, setShowAuthForm] = useState<boolean>(false);
 
   const [authOptions, setAuthOptions] = useState<GoogleUser>();
+
+  let body = null;
+
+  if (loading) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <div className="col-span-1 text-white">
+          Logo and intro here (come back to design)
+        </div>
+        <div className="col-span-1 my-auto p-8">
+          <h1 className="mb-3 text-2xl font-bold text-primary-100 text-center">
+            Welcome
+          </h1>
+          {!showAuthForm ? null : (
+            <AuthFormController authOptions={authOptions} />
+          )}
+          {showAuthForm ? null : (
+            <GoogleOAuthProvider clientId={googleClientId}>
+              <GoogleAuthButtonController
+                getGoogleUser={(googleUser) => {
+                  setAuthOptions(googleUser);
+                  setShowAuthForm(true);
+                }}
+              />
+            </GoogleOAuthProvider>
+          )}
+        </div>
+      </>
+    );
+  } else {
+    router.push('/dashboard');
+  }
 
   return (
     <div
       className="grid gap-5 justify-center h-screen w-screen"
       style={{ gridTemplateColumns: '640px 640px', margin: '0 auto' }}
     >
-      <div className="col-span-1 text-white">
-        Logo and intro here (come back to design)
-      </div>
-      <div className="col-span-1 my-auto p-8">
-        <h1 className="mb-3 text-2xl font-bold text-primary-100 text-center">
-          Welcome
-        </h1>
-        {!showAuthForm ? null : (
-          <AuthFormController authOptions={authOptions} />
-        )}
-        {showAuthForm ? null : (
-          <GoogleOAuthProvider clientId={googleClientId}>
-            <GoogleAuthButtonController
-              getGoogleUser={(googleUser) => {
-                setAuthOptions(googleUser);
-                setShowAuthForm(true);
-              }}
-            />
-          </GoogleOAuthProvider>
-        )}
-      </div>
+      {body}
     </div>
   );
 };
