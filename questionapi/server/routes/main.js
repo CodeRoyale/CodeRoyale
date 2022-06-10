fc9014535268a10fc9e22a2d278c4470ab13a070
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-06-11 00:59:34 +0530

    Removed qapi subfolder

--- questionapi/server/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const route = express.Router();
-
-route.get('/', (req, res) =>
-  res.send('CodeRoyale Question API Server is up and running')
-);
-
-module.exports = route;
