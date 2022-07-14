26020c0b90eb5eb967cb5c4d68db0635e4278322
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-07-14 23:01:19 +0530

    feat(frontend): improved chat component css position

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,61 +1,23 @@
 import { Form, Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch } from '../Switch';
 import { ChatInput } from './ChatInput';
 
 export const Chat: React.FC<{}> = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+
   return (
-    <div className="w-full h-full mt-8">
-      <div className="absolute bottom-[98px] px-4 flex flex-col-reverse w-full h-full border-t-[230px] border-transparent overflow-auto mt-[150px] scrollbar-thin scrollbar-thumb-primary-700">
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
-        <div className="text-white">Joel</div>
+    <div className="bottom-0 left-0 w-full mt-8 bg-primary-800 rounded-md">
+      <div
+        className="relative px-4 py-2 w-full h-auto overflow-y-hidden hover:overflow-y-auto scrollbar-thin scrollbar-thumb-primary-700"
+        style={{ height: 'calc(80vh - 100px)' }}
+      >
+        <div className="text-primary-300 text-sm">Welcome to chat!</div>
+        {messages.map((message, key) => (
+          <div key={key} className="text-white">
+            {message}
+          </div>
+        ))}
       </div>
       <Formik
         initialValues={{
@@ -63,15 +25,17 @@ export const Chat: React.FC<{}> = () => {
           toTeam: false,
         }}
         onSubmit={(values, { setErrors }) => {
+          setMessages((old) => [...old, values.message]);
           console.log(values);
         }}
       >
-        {({ isSubmitting, values, setFieldValue }) => (
-          <Form className="absolute bottom-0 left-0 w-full p-4">
+        {({ values, setFieldValue }) => (
+          <Form className="w-full pt-2 pb-4 px-4">
             <ChatInput
               name="message"
               placeholder="Send a message"
               type="text"
+              autoComplete="off"
             />
             <div className="mt-3">
               <Switch
