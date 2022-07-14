18caa828d3cab9ec92e1203a5f8b846411f6b1f5
Rec0iL99 <joelmathewkoshy@gmail.com>
2022-07-15 00:09:07 +0530

    feat(frontend): scroll to bottom of chatMessages when new message appears

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,23 +1,37 @@
+import React, { useState, useRef, useEffect } from 'react';
 import { Form, Formik } from 'formik';
-import React, { useState } from 'react';
 import { Switch } from '../Switch';
 import { ChatInput } from './ChatInput';
 
 export const Chat: React.FC<{}> = () => {
   const [messages, setMessages] = useState<string[]>([]);
+  const chatRef = useRef<null | HTMLDivElement>(null);
+  const bottomRef = useRef<null | HTMLDivElement>(null);
+
+  // scroll to bottom of chat window when new chat message comes in
+  useEffect(() => {
+    // dont scroll to bottom if user has scrolled up
+    // if (
+    //   chatRef.current?.getBoundingClientRect().bottom! <= window.innerHeight
+    // ) {
+    //   bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+    // }
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   return (
     <div className="bottom-0 left-0 w-full mt-8 bg-primary-800 rounded-md">
       <div
-        className="relative px-4 py-2 w-full h-auto overflow-y-hidden hover:overflow-y-auto scrollbar-thin scrollbar-thumb-primary-700"
+        className="relative px-4 py-2 w-full h-auto overflow-y-auto scrollbar-thin scrollbar-thumb-primary-700"
         style={{ height: 'calc(100vh - 272px)' }}
       >
         <div className="text-primary-300 text-sm">Welcome to chat!</div>
         {messages.map((message, key) => (
-          <div key={key} className="text-white break-words">
+          <div ref={chatRef} key={key} className="text-white break-words">
             {message}
           </div>
         ))}
+        <div ref={bottomRef}></div>
       </div>
       <Formik
         initialValues={{
