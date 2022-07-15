5a628ae854b0d4c41a3cb8761677edec9c7ae1d2
vuld0 <chiragsonubablani@karunya.edu.in>
2022-07-15 19:25:44 +0530

    feat(lobby): new function: joinedRoom()

---
+++ lobby/src/controllers/roomController/joinRoom.ts
@@ -0,0 +1,51 @@
+import { ROOM_PREFIX } from "../../utils/constants";
+import { DataFromServer, Room } from "../../types/types";
+import { getUser, updateUser } from "../userController";
+import { JOINED_ROOM, ROOM_UPDATED } from "src/socketActions/serverActions";
+
+export const joinRoom = async (
+    roomId: string,
+    { socket, redis, currentUserId }: DataFromServer
+) => {
+    const getUserResult = await getUser(currentUserId, redis!);
+    const user = getUserResult.data;
+    const roomResult = await redis?.get(ROOM_PREFIX + roomId);
+    let room: Room;
+
+    if (roomResult) {
+        room = JSON.parse(roomResult);
+    } else {
+        return false;
+    }
+
+    // (only run if room doesn't exists) and (user is allowed if private) and (space is there)
+    //! privateList needs to be added
+    if (
+        !room &&
+        !room['config']['private']  &&
+        room['state']['currMemberCount'] > room['config']['maxMembers']
+    ) {
+        return { status: 0, error: "The User doesn't meet the specifications" };
+    }
+
+    // User is already in a different room do not allow
+    if (user?.userId) {
+        return { status: 1, error: 'User already in room' };
+    }
+
+    room.state.currMemberCount += 1;
+    await redis?.set(ROOM_PREFIX + roomId, JSON.stringify(room));
+
+    await updateUser(user!, redis!);
+
+    socket.join(roomId);
+    socket.to(roomId).emit(ROOM_UPDATED, {
+      type: JOINED_ROOM,
+      data: { currentUserId },
+    });
+    console.log(`userId:${currentUserId} joined from ${roomId}`);
+    return room;
+    
+}; 
+    
+    
--- lobby/src/socketActions/serverActions.ts
+++ lobby/src/socketActions/serverActions.ts
@@ -1,3 +1,4 @@
 // update client about updated room
 // currently this will only track members updation
 export const ROOM_UPDATED = "ROOM_UPDATED";
+export const JOINED_ROOM = "JOINED_ROOM";
